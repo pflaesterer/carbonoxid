dbe7cbb6c638ca527290a891b62935800493e7d9
Philip Pflästerer <philip@pflaesterer.org>
2018-07-23 09:38:08 +0200

    Added throw of an exception in case of invalid email-address.

--- Core/Email.php
+++ Core/Email.php
@@ -10,7 +10,22 @@ class Email extends Email_parent
         $bcc    = $config->getConfigParam('carbonoxid_mail_bcc_email',  \OxidEsales\Eshop\Core\Registry::getConfig()->getShopId(), 'module:carbonoxid');
         $name   = $config->getConfigParam('carbonoxid_mail_bcc_name',   \OxidEsales\Eshop\Core\Registry::getConfig()->getShopId(), 'module:carbonoxid');
 
-        if(filter_var($bcc, FILTER_VALIDATE_EMAIL))
+        if(!filter_var($bcc, FILTER_VALIDATE_EMAIL))
+        {
+            // throw exception
+            $ox = oxNew(\OxidEsales\Eshop\Core\Exception\StandardException::class);
+            $ex->setMessage('Not a valid email-address.');
+
+            if($this->isDebugModeEnabled())
+            {
+                throw $ex;
+            }
+            else
+            {
+                $ex->debugOut();
+            }
+        }
+        else
         {
             parent::AddBCC($bcc, $name);
         }
